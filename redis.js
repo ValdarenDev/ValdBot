49ff5f5e72823d13a2fc87b5fc5f25f555704307
ValdarenDev <bdog0804@gmail.com>
2026-04-02 10:03:17 -0500

    Fix for .env in redis.js

--- redis.js
+++ redis.js
@@ -1,10 +1,5 @@
 import { Redis } from "@upstash/redis";
 
-if (process.env.NODE_ENV !== "production") {
-    const dotenv = await import("dotenv");
-    dotenv.config({ override: false });
-}
-
 console.log("UPSTASH_REDIS_REST_URL =", JSON.stringify(process.env.UPSTASH_REDIS_REST_URL));
 console.log("UPSTASH_REDIS_REST_TOKEN =", JSON.stringify(process.env.UPSTASH_REDIS_REST_TOKEN));
 
